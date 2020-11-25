9a8ee22455e0527579b0dc6de9883e95cd14b2f9
Nicolas Coureau <ncoureau@gmail.com>
2020-11-26 00:44:26 +0100

    Remplissage de toutes les parties + mise en place css

--- js/index.js
+++ js/index.js
@@ -1,5 +1,18 @@
-window.onload = function () {
+// Toggle between showing and hiding the sidebar when clicking the menu icon
+var mySidebar = document.getElementById("mySidebar");
 
-
-
+function w3_open() {
+    if (mySidebar.style.display === 'block') {
+        mySidebar.style.display = 'none';
+    } else {
+        mySidebar.style.width = '100%';
+        mySidebar.style.display = 'block';
+    }
 }
+
+// Close the sidebar with the close button
+function w3_close() {
+    mySidebar.style.display = "none";
+}
+
+
