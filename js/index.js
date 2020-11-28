d6cf5c4f549890345cee690b49a617bc79b12631
Nicolas Coureau <ncoureau@gmail.com>
2020-11-28 01:30:19 +0100

    Mise en place d'animations

--- js/index.js
+++ js/index.js
@@ -9,10 +9,40 @@ function w3_open() {
         mySidebar.style.display = 'block';
     }
 }
+// animation on the title
+anime.timeline({loop: true})
+    .add({
+        targets: '.ml15 .word',
+        scale: [14,1],
+        opacity: [0,1],
+        easing: "easeOutCirc",
+        duration: 1500, // Temps pour apparaitre
+        delay: (el, i) => 1500 * i
+    }).add({
+    targets: '.ml15',
+    opacity: 0,
+    duration: 1500, // Temps pour disparaitre
+    easing: "easeOutExpo",
+    delay: 3000 // Temps d'affichage
+});
 
-// Close the sidebar with the close button
-function w3_close() {
-    mySidebar.style.display = "none";
+// animation on the achievements section
+function rotate(event) {
+    anime({
+        targets: event.target,
+        //loop: true,
+        rotate: '1turn',
+    });
 }
 
+document.getElementById('img-1').addEventListener('mouseover', rotate);
+document.getElementById('img-2').addEventListener('mouseover', rotate);
+document.getElementById('img-3').addEventListener('mouseover', rotate);
+document.getElementById('img-4').addEventListener('mouseover', rotate);
+document.getElementById('img-5').addEventListener('mouseover', rotate);
+document.getElementById('img-6').addEventListener('mouseover', rotate);
+document.getElementById('img-7').addEventListener('mouseover', rotate);
+document.getElementById('img-8').addEventListener('mouseover', rotate);
+document.getElementById('img-9').addEventListener('mouseover', rotate);
+document.getElementById('img-10').addEventListener('mouseover', rotate);
 
