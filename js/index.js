5fe46b1181ccb2cfa7c22f789c7f97ed683635d7
Nicolas Coureau <ncoureau@gmail.com>
2020-11-30 21:58:54 +0100

    Suppression animation titre appareil mobile

--- js/index.js
+++ js/index.js
@@ -15,21 +15,27 @@ function w3_close() {
 }
 
 // animation on the title
-anime.timeline({loop: true})
-    .add({
-        targets: '.ml15 .word',
-        scale: [14,1],
-        opacity: [0,1],
-        easing: "easeOutCirc",
-        duration: 1500, // Temps pour apparaitre
-        delay: (el, i) => 1500 * i
-    }).add({
-    targets: '.ml15',
-    opacity: 0,
-    duration: 1500, // Temps pour disparaitre
-    easing: "easeOutExpo",
-    delay: 3000 // Temps d'affichage
-});
+if (window.matchMedia("(min-width: 601px)").matches) {
+    //The viewport is less than, or equal to, 700 pixels wide
+    anime.timeline({loop: true})
+        .add({
+            targets: '.ml15 .word',
+            scale: [14,1],
+            opacity: [0,1],
+            easing: "easeOutCirc",
+            duration: 1500, // Temps pour apparaitre
+            delay: (el, i) => 1500 * i
+        }).add({
+        targets: '.ml15',
+        opacity: 0,
+        duration: 1500, // Temps pour disparaitre
+        easing: "easeOutExpo",
+        delay: 3000 // Temps d'affichage
+    });
+} else {
+    //The viewport is greater than 700 pixels wide
+    console.log("No animation on the title on cell phone");
+}
 
 // animation on the achievements section
 function rotate(event) {
